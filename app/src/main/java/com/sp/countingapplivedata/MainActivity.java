92797fd3b59468a0ddbebb058e215619c07f3eb8
Saikrishna Pawar <saikrishna.pawar@mavenir.com>
2020-08-07 14:39:44 -0500

    Added data binding mechanism

--- app/src/main/java/com/sp/countingapplivedata/MainActivity.java
+++ app/src/main/java/com/sp/countingapplivedata/MainActivity.java
@@ -17,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
         final ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
