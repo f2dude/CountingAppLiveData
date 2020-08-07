11f9c1ad31a34d6a789ebd03e09415d6015317cc
Saikrishna Pawar <saikrishna.pawar@mavenir.com>
2020-08-07 14:36:06 -0500

    Added data binding mechanism

--- app/src/main/java/com/sp/countingapplivedata/MainActivity.java
+++ app/src/main/java/com/sp/countingapplivedata/MainActivity.java
@@ -1,6 +1,7 @@
 package com.sp.countingapplivedata;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -9,6 +10,7 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.sp.countingapplivedata.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,18 +19,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView textView = findViewById(R.id.txt1);
-        FloatingActionButton fab = findViewById(R.id.btn1);
+        final ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
         final MainActivityViewModel model = new ViewModelProvider(this).get(MainActivityViewModel.class);
         model.getInitialCount().observe(this, new Observer<Integer>() {
             @Override
             public void onChanged(Integer integer) {
-                textView.setText("Count is: " + integer);
+                binding.txt1.setText("Count is: " + integer);
             }
         });
 
-        fab.setOnClickListener(new View.OnClickListener() {
+        binding.btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 model.getCurrentCount();
